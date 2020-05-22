1d9d57cf6f4733d0d3868e664b885c83cf6288ae
Paul Broadwith <paul@pauby.com>
2020-05-22 13:15:30 +0100

    Remove disabling IE security, WU and PS Profile

--- puppet/modules/set_windows_autologon/manifests/init.pp
+++ puppet/modules/set_windows_autologon/manifests/init.pp
@@ -31,38 +31,4 @@ class set_windows_autologon {
   registry_value { 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\AutoAdminLogonCount':
     ensure => absent
   }
-
-  #
-  # Prepare Windows
-  #
-
-  ie_esc { 'IE ESC Configuration':
-    ensure => absent
-  }
-
-  # Stop and disable Windows Updates
-  service { 'wuauserv':
-    ensure => stopped,
-    enable => manual
-  }
-
-  registry_key { 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU':
-    ensure => present
-  }
-
-  registry_value { 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\NoAutoUpdate':
-    ensure => present,
-    type   => dword,
-    data   => 1
-  }
-
-  # create Windows PowerShell profile
-  file { 'c:\users\vagrant\Documents\WindowsPowerShell':
-    ensure => directory
-  }
-
-  file { 'c:\users\vagrant\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1':
-    ensure  => file,
-    require => File['c:\users\vagrant\Documents\WindowsPowerShell'],
-  }
 }
