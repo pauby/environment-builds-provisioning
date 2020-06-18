e30488cc3f9d38cadfc20822c81c203fd73860ac
Paul Broadwith <pauby@users.noreply.github.com>
2020-06-18 10:48:25 +0100

    (maint: Add comment to clarify when to be used

--- puppet/modules/pauby_vagrant_provision/manifests/win_chocolatey_source_local_add.pp
+++ puppet/modules/pauby_vagrant_provision/manifests/win_chocolatey_source_local_add.pp
@@ -1,3 +1,5 @@
+# this could easily be done with a simple `chocolateysource` but it's here really for the defaults to be used with one
+# line in the provisioning file.
 class pauby_vagrant_provision::win_chocolatey_source_local_add (
   String $source_local_packages_path = 'c:/resources/packages'
 ) {
