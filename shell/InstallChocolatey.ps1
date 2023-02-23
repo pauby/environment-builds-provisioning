f66568b0cd2a006c858a9d4d5dfad56a285988dd
Paul Broadwith <paul@pauby.com>
2023-02-23 14:49:04 +0000

    (refact) Don't error if reg keys missing

--- shell/InstallChocolatey.ps1
+++ shell/InstallChocolatey.ps1
@@ -4,18 +4,21 @@ Param (
     $UseLocalSource,
 
     [string]
+    $LocalSourcePath = "$($env:SystemDrive)\resources\packages",
+
+    [string]
     $Version,
 
     [switch]
     $Force
 )
 
-# only install Chocolatey if it's not installed
-# Note that in the newer versions of Chocolatey the install script will stop
-# if it detects files / folders in the default Chocolatey install location.
-# So Chocolatey won't be installed again but all of the configuration below
-# will run.
-if (-not (Get-Command -Name 'choco' -ErrorAction  SilentlyContinue) -or $Force.IsPresent) {
+#! only install Chocolatey if it's not installed
+#! Note that in the newer versions of Chocolatey the install script will stop
+#! if it detects files / folders in the default Chocolatey install location.
+#! So Chocolatey won't be installed again but all of the configuration below
+#! will run.
+if (-not (Get-Command -Name 'choco' -ErrorAction  SilentlyContinue)) {
 
     # PowerShell will not set this by default (until maybe .NET 4.6.x). This
     # will typically produce a message for PowerShell v2 (just an info
@@ -36,7 +39,17 @@ if (-not (Get-Command -Name 'choco' -ErrorAction  SilentlyContinue) -or $Force.I
     Set-ExecutionPolicy Bypass -Scope Process -Force
     Invoke-WebRequest 'https://chocolatey.org/install.ps1' -UseBasicParsing -OutFile $installScript
     & $installScript
+}
+else {
+    # If Chocolatey CLI is already installed, update it to the latest version
+    choco upgrade chocolatey --no-progress --yes
+}
 
+# If we get here Chocolatey CLI must be installed (either it was already installed, or it was installed above).
+# Just in case there was an error with the install above, lets check again.
+# The -Force parameter is kind of redundant here as we will always execute this code as choco.exe will always be
+# installed
+if ((Get-Command -Name 'choco' -ErrorAction SilentlyContinue) -or $Force.IsPresent) {
     #Update-SessionEnvironment
     choco feature enable --name='autouninstaller'
     # - not recommended for production systems:
@@ -45,16 +58,18 @@ if (-not (Get-Command -Name 'choco' -ErrorAction  SilentlyContinue) -or $Force.I
     choco feature enable --name='logEnvironmentValues'
     choco feature enable --name='useRememberedArgumentsForUpgrades'
     choco feature disable --name='exitOnRebootDetected'
-    # Set Configuration
-    choco config set cacheLocation $env:ALLUSERSPROFILE\chocolatey\choco-cache
-    choco config set commandExecutionTimeoutSeconds 14400
-    choco feature disable -n=showDownloadProgress
-    choco feature disable -n=LogValidationResultsOnWarnings
 
-    if ($UseLocalSource.IsPresent) {
-        Write-Host "Using local '$($env:SystemDrive)\packages' folder as priority 1 install location."
+    # Set Configuration
+    choco config set cacheLocation "$env:ALLUSERSPROFILE\chocolatey\choco-cache"
+    choco config set commandExecutionTimeoutSeconds 14400
+    choco feature disable -n='showDownloadProgress'
+    choco feature disable -n='LogValidationResultsOnWarnings'
+
+    # Only add the local packages folder as a source if we ask it to (-UseLocalSource) and it exists
+    if ($UseLocalSource.IsPresent -and (Test-Path -Path $LocalSourcePath)) {
+        Write-Host "Using local packages folder '$LocalSourcePath' folder as priority 1 install location."
         # Sources - Add internal repositories
-        choco source add --name="'local'" --source="'$($env:SystemDrive)\resources\packages'" --priority="'1'" --bypass-proxy --allow-self-service
+        choco source add --name="'local'" --source="'$LocalSourcePath'" --priority="'1'" --bypass-proxy --allow-self-service
 
         # Sources - change priority of community repository
         #Write-Output "Using Chocolatey Community Repository as priority 20 install location."
@@ -67,7 +82,6 @@ if (-not (Get-Command -Name 'choco' -ErrorAction  SilentlyContinue) -or $Force.I
         choco upgrade chocolatey --version $Version --allow-downgrade --prerelease -y --no-progress --limit-output
     }
 }
-else {
-    Write-Host 'Chocolatey already installed. Nothing to do.'
-    choco --version
-}
+
+Write-Host 'Available Chocolatey CLI version.'
+choco --version
