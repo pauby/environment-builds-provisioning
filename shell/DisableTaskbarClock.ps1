8a5caf9933803b07b5d46867d8971e3f00622bbb
Paul Broadwith <paul@pauby.com>
2023-02-23 14:49:41 +0000

    (fix) Don't error if reg keys missing

--- shell/DisableTaskbarClock.ps1
+++ shell/DisableTaskbarClock.ps1
@@ -1,4 +1,18 @@
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name HideClock -Value 1
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name HideClock -Value 1
+$keys = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer', 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer'
 
-Stop-Process -Name "explorer"
+try {
+    $keys | ForEach-Object {
+        if (-not (Test-Path -Path $_)) {
+            New-Item -Path $_ | Out-Null
+        }
+        Set-ItemProperty -Path $_ -Name 'HideClock' -Value 1
+    }
+}
+catch {
+    # lets not write an error as it will stop the Vagrant provisioning
+    Write-Warning 'There was a problem writing one of the registry keys.'
+    $error[0]
+}
+
+# when this runs the explorer process may not be available, or Vagrant doesn't have access to it. Ignore errors.
+Get-Process -Name 'explorer' -ErrorAction SilentlyContinue | Stop-Process
